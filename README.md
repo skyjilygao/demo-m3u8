e2ad61892d393b5eff1770a84d83c602a554ceed
Administrator <gaoqihua@empowerwin.com>
2018-12-17 18:12:41 +0800

    使用配置文件

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # demo-m3u8
 mp4转m3u8 demo
+本项目主要是mp4转m3u8相关调用
--- src/main/java/com/skyjilygao/springboot/demo/controller/VideoController.java
+++ src/main/java/com/skyjilygao/springboot/demo/controller/VideoController.java
@@ -1,6 +1,7 @@
 package com.skyjilygao.springboot.demo.controller;
 
 import com.skyjilygao.util.VideoConvert;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,16 +18,24 @@ import java.io.FileNotFoundException;
 public class VideoController {
     VideoConvert videoConvert;
 
+    @Value("${video.source}")
+    private String source;
+    @Value("${video.target}")
+    private String target;
+    @Value("${ffmpeg:ffmpeg}")
+    private String ffmpeg;
+
+
     @RequestMapping("")
     public String main(){
         return "hello video";
     }
     @RequestMapping("start")
     public String start() throws FileNotFoundException, InterruptedException {
-        String source = "E:\\test\\video2.mp4";
-        String target = "E:\\test\\video2.m3u8";
+        String source = this.source;//"E:\\test\\video2.mp4";
+        String target = this.target;//"E:\\test\\video2.m3u8";
 //        String ffmpegPath = "D:\\!Soft\\ffmpeg-20181127-1035206-win64-static\\ffmpeg-20181127-1035206-win64-static\\bin\\ffmpeg.exe";
-        String ffmpegPath = "ffmpeg";
+        String ffmpegPath = this.ffmpeg;//"ffmpeg";
         VideoConvert convert = new VideoConvert(ffmpegPath);
         convert.start(source, target);
         videoConvert = convert;
