81cb868a4b7295d84c8a93f86692d42237f50a68
Administrator <gaoqihua@empowerwin.com>
2018-12-17 17:56:51 +0800

    test

--- src/test/java/com/skyjilygao/springboot/demo/DemoM3u8ApplicationTests.java
+++ src/test/java/com/skyjilygao/springboot/demo/DemoM3u8ApplicationTests.java
@@ -1,16 +1,23 @@
 package com.skyjilygao.springboot.demo;
 
+import com.skyjilygao.springboot.demo.controller.VideoController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileNotFoundException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoM3u8ApplicationTests {
 
+	@Autowired
+	private VideoController videoController;
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws FileNotFoundException, InterruptedException {
+		videoController.start();
 	}
 
 }
